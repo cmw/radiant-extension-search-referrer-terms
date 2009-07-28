6b0b7e87935341b6c1574b3605a8ee35bb0020a3
Christian Weis <christian.weis@gmail.com>
2009-07-29 00:21:11 +0200

    fix for 0.8.0

--- search_referrer_terms_extension.rb
+++ search_referrer_terms_extension.rb
@@ -1,4 +1,4 @@
-require_dependency 'application'
+require_dependency 'application_controller'
 class SearchReferrerTermsExtension < Radiant::Extension
   version "0.1"
   description "Extracts out the search terms entered into google/yahoo/msn/etc that have referred a user to your site "
